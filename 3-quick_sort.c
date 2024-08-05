8ec58539977413ce94878cdbb4a8302b0b268c45
TheSnewby <stephen.j.newby@gmail.com>
2024-08-05 17:41:26 -0500

    changed the new variable to keep track of original head of array

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -29,7 +29,7 @@ void quick_sort(int *array, size_t size)
 int helper_qs(int *array, int low, int high, size_t size)
 {
 	int pivot, i, j, swap;
-	size_t original_size = size;
+	int *head = array;
 
 	pivot = array[high];
 	i = low - 1;
@@ -42,12 +42,12 @@ int helper_qs(int *array, int low, int high, size_t size)
 			swap = array[j];
 			array[j] = array[i];
 			array[i] = swap;
-			print_array(array, original_size);
+			print_array(head, size);
 		}
 	}
 	swap = array[high];
 	array[high] = array[i + 1];
 	array[i + 1] = swap;
-	print_array(array, original_size);
+	print_array(head, size);
 	return (i + 1);
 }
