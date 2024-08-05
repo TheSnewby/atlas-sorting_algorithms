e1a386d570d5fa52fc5333d25173792ca0aa9655
TheSnewby <stephen.j.newby@gmail.com>
2024-08-05 16:30:47 -0500

    selection-sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of ints ascendingly with selection sort
+ * @array: array of integers
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size);
+{
+	int i, j, min, swap;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = 0; i < size; i++)
+		{
+			if (array[j] < array[min])
+				min = j; /* index of new min */
+		}
+		if (min != i)
+		{
+			swap = array[i];
+			array[i] = array[min];
+			array[min] = swap;
+		}
+	}
+}
