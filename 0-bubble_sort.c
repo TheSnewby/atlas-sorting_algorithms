07e76d21301988eff3510afc83bf35fc9ca283f5
TheSnewby <stephen.j.newby@gmail.com>
2024-08-05 09:37:01 -0500

    added printing step for every swap

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,20 +7,24 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, newn, swap;
+	size_t i, newn, swap, swapped, orig_size = size;
 
 	while (size > 1)
 	{
 		newn = 0;
 		for (i = 0; i <= size - 1; i++)
 		{
+			swapped = 0;
 			if (array[i - 1] > array[i])
 			{
 				swap = array[i];
 				array[i] = array[i - 1];
 				array[i - 1] = swap;
 				newn = i;
+				swapped = 1;
 			}
+			if (swapped == 1)
+				print_array(array, orig_size);
 		}
 		size = newn;
 	}
