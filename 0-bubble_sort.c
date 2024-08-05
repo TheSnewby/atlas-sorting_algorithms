5a1788aa7f4698a3a8bd5013f165033368c203d8
TheSnewby <stephen.j.newby@gmail.com>
2024-08-05 09:22:06 -0500

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * performs a bubble sort on sortable data in ascending order
+ * @array: array of integers
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, newn, swap;
+
+	while (size > 1)
+	{
+		newn = 0;
+		for (i = 0; i <= size - 1, i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				swap = array[i];
+				array[i] = array[i - 1];
+				array[i - 1] = swap;
+				newn = i;
+			}
+		}
+	}
+}
