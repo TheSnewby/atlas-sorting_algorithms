7ee326b90298def3b5963cb27638cb390d373543
TheSnewby <stephen.j.newby@gmail.com>
2024-08-05 16:36:55 -0500

    changed i to j in second loop's 2nd and 3rd arguments

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,7 +12,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < (int)size - 1; i++)
 	{
 		min = i;
-		for (j = i + 1; i < (int)size; i++)
+		for (j = i + 1; j < (int)size; j++)
 		{
 			if (array[j] < array[min])
 				min = j; /* index of new min */
