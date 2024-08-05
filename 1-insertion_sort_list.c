408f47b3cff00f5fd18d512940acc88a33018967
TheSnewby <stephen.j.newby@gmail.com>
2024-08-05 12:08:16 -0500

    optimized insertion-sort?

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,46 +6,46 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL, *head = *list, *temp = NULL;
+	listint_t *current = NULL, *head = *list, *temp = NULL, *current_next;
 
 	if (*list == NULL || (*list)->next == NULL)
-		;
-	else
+		return;
+
+	current = head->next;
+	while (current != NULL)
 	{
-		while (*list != NULL)
+		current_next = current->next; /* tracks current's next node */
+		if (current->n < head->n) /* check head first */
 		{
-			current = (*list)->next;
-			if (current->n < head->n) /* check head first */
-			{
-				updateCurrentSurroundingNodes(list, current);
-				current->next = head; /* update current's next & prev */
-				current->prev = NULL;
-				head->prev = current;
-				head = current; /* update head */
-				print_list(list);
-			}
-			else
-			{
-				/* insert current element in between others if not largest */
-				temp = head;
-				while (temp != NULL && temp != current)
-				{	/* updates current's surrounding nodes */
-					if (current->n > temp->n && current->n < temp->next->n)
-					{
-						updateCurrentSurroundingNodes(list, current);
-						current->next = temp->next;
-						current->prev = temp;
-						current->next->prev = current;
-						current->prev->next = current;
-						print_list(list);
-						break;
-					}
-					temp = temp->next; /* iterate */
+			updateCurrentSurroundingNodes(list, current);
+			current->next = head; /* update current's next & prev */
+			current->prev = NULL;
+			head->prev = current;
+			head = current; /* update head */
+			print_list(head);
+		}
+		else
+		{
+			/* insert current element in between others if not largest */
+			temp = current->prev; /* iterator */
+			while (temp != NULL)
+			{	/* updates current's surrounding nodes */
+				if (current->n > temp->n && current->n < temp->next->n)
+				{
+					updateCurrentSurroundingNodes(list, current);
+					current->next = temp->next;
+					current->prev = temp;
+					current->next->prev = current;
+					current->prev->next = current;
+					print_list(head);
+					break;
 				}
+				temp = temp->prev; /* iterate */
 			}
 		}
-		/* *list = head; consider for final action */
+		current = current_next;
 	}
+	*list = head; /* consider for final action */
 }
 
 /**
