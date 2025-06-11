a3dcb4063386cc8fc242fbb3a6baabb5dbf147e7
TheSnewby <stephen.j.newby@gmail.com>
2025-06-11 10:17:09 -0500

    added printing, replaced malloced variable with fixed array

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,17 +3,33 @@
 /**
  * merge - merges arrays and sorts elements from smallest to largest
  * @arr: array of ints to be sorted
- * @temp: temporary array
  * @sizeL: size of left array
  * @sizeR: size of right array
  */
-void merge(int *arr, int *temp, size_t sizeL, size_t sizeR)
+void merge(int *arr, size_t sizeL, size_t sizeR)
 {
-	size_t i = 0, l = 0, r = 0;
+	size_t i, l = 0, r = 0;
+	int temp[1024];
 
-	/* consider using malloc in order to handle very large arrays */
 	memcpy(temp, arr, sizeof(int) * (sizeL + sizeR));
 
+	printf("Merging...\n[left]: ");
+	for (i = 0; i < sizeL; i++)
+	{
+		if (i)
+			printf(", ");
+		printf("%d", temp[i]);
+	}
+	printf("\n[right]: ");
+	for (i; i < sizeL + sizeR; i++)
+	{
+		if (i > sizeL)
+			printf(", ");
+		printf("%d", temp[i]);
+	}
+	printf("\n");
+
+	i = 0;
 	while (l < sizeL && r < sizeR)
 	{
 		if (temp[l] < temp[r + sizeL])
@@ -28,15 +44,18 @@ void merge(int *arr, int *temp, size_t sizeL, size_t sizeR)
 
 	while (r < sizeR)
 		arr[i++] = temp[r++ + sizeL];
+
+	printf("[Done]: ");
+	print_array(arr, sizeL + sizeR);
+	printf("\n");
 }
 
 /**
  * merge_sort_algo - recursive helper to merge_sort
  * @array: array to be sorted
- * @temp: temporary array for handling
  * @size: size of array
  */
-void merge_sort_algo(int *array, int *temp, size_t size)
+void merge_sort_algo(int *array, size_t size)
 {
 	size_t midpoint, rightSize;
 
@@ -49,7 +68,7 @@ void merge_sort_algo(int *array, int *temp, size_t size)
 	merge_sort(array, midpoint);
 	merge_sort(array + midpoint, rightSize);
 
-	merge(array, temp, midpoint, rightSize);
+	merge(array, midpoint, rightSize);
 }
 /**
  * merge_sort - top-down merge sort algorithm
@@ -63,7 +82,5 @@ void merge_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 
-	temp = (int *)malloc(sizeof(int) * size);
-	merge_sort_algo(array, temp, size);
-	free(temp);
+	merge_sort_algo(array, size);
 }
